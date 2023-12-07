92e94baa454f450306adddb1ec67118b15ee1e0d
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-07 10:38:42 +0000

    locked bosse

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,7 +4,7 @@ This module is for function to onlock
 """
 
 
-def can_unlock_all(boxes):
+def canUnlockAll(boxes):
     """
     Determine if all boxes can be opened.
 
@@ -16,17 +16,9 @@ def can_unlock_all(boxes):
     Returns:
         bool: True if all boxes can be opened, False otherwise.
     """
-
-    # Initialize a set to keep track of the boxes that can be opened
-    unlocked_boxes = {0}
-
-    # Iterate through the unlocked boxes and find new keys
-    for box in unlocked_boxes:
-        for key in boxes[box]:
-            # Check if the key is within the valid
-            # range and the box is not already unlocked
-            if 0 <= key < len(boxes) and key not in unlocked_boxes:
-                unlocked_boxes.add(key)
-
-    # Check if all boxes can be opened
-    return len(unlocked_boxes) == len(boxes)
+    keys = [0]
+    for n in keys:
+        for key in boxes[n]:
+            if key not in keys and key < len(boxes):
+                keys.append(key)
+    return len(keys) == len(boxes)
