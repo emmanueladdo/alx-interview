f31d6d0c9159adab31c13b9f0ff26158d122804c
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-14 01:00:47 +0000

    minor operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+The module Produces the minimum time
+it takes to write operations of 'copy' and 'paste'
+"""
+
+
+def minOperations(n):
+    """
+    A method that calculates the fewest number of operations needed
+    to result in exactly nH characters
+    """
+    if n <= 1:
+        return 0
+
+    """ loop for n number of times """
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(int(n / i)) + i
