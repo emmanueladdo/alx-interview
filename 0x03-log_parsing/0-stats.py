a2d31fb2bbb3b4a73caf035dc72dc183cd06d2b2
emmanueladdo <asememmanueladd@gmail.com>
2023-12-20 15:47:21 +0000

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,42 +8,41 @@ prints the statuscount
 import sys
 
 if __name__ == "__main__":
-    status_code = {"200": 0,
-                   "301": 0,
-                   "400": 0,
-                   "401": 0,
-                   "403": 0,
-                   "404": 0,
-                   "405": 0,
-                   "500": 0}
+    st_code = {"200": 0,
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
 
-    line_count = 1
-    total_file_size = 0
+    count = 1
+    file_size = 0
 
-    def parse_log_line(log_line):
-        # Read, parse, and get data
+    def parse_line(line):
         try:
-            parsed_line = log_line.split()
-            code = parsed_line[-2]
-            if code in status_code:
-                status_code[code] += 1
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in st_code.keys():
+                st_code[status_code] += 1
             return int(parsed_line[-1])
         except Exception:
             return 0
 
-    def print_statistics():
-        # Print status in ascending order
-        print("File size: {}".format(total_file_size))
-        for key in sorted(status_code.keys()):
-            if status_code[key]:
-                print("{}: {}".format(key, status_code[key]))
+    def print_stats():
+        print("File size: {}".format(file_size))
+        for key in sorted(st_code.keys()):
+            if st_code[key]:
+                print("{}: {}".format(key, st_code[key]))
 
     try:
         for line in sys.stdin:
-            total_file_size += parse_log_line(line)
-            if line_count % 10 == 0:
-                print_statistics()
-            line_count += 1
+            file_size += parse_line(line)
+            if count % 10 == 0:
+                print_stats()
+            count += 1
     except KeyboardInterrupt:
-        print_statistics()
+        print_stats()
         raise
+    print_stats()
