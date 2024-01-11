8eebb2c4de2465ffae75e9da7227c2e5d3240795
emmanueladdo <asememmanueladd@gmail.com>
2024-01-11 23:48:34 +0000

    the queen

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -25,19 +25,19 @@ if __name__ == "__main__":
             print(queens_positions)
         else:
             for col in range(n):
-                current_placement = [row, col]
-                if is_valid_placement(queens_positions, current_placement):
-                    queens_positions.append(current_placement)
+                current_place = [row, col]
+                if is_valid_placement(queens_positions, current_place):
+                    queens_positions.append(current_place)
                     solve_queens(row + 1, n, queens_positions)
-                    queens_positions.remove(current_placement)
+                    queens_positions.remove(current_place)
 
-    def is_valid_placement(queens_positions, current_placement):
+    def is_valid_placement(queens_positions, current_place):
         for queen in queens_positions:
-            if queen[1] == current_placement[1]:
+            if queen[1] == current_place[1]:
                 return False
-            if (queen[0] + queen[1]) == (current_placement[0] + current_placement[1]):
+            if (queen[0] + queen[1]) == (current_place[0] + current_place[1]):
                 return False
-            if (queen[0] - queen[1]) == (current_placement[0] - current_placement[1]):
+            if (queen[0] - queen[1]) == (current_place[0] - current_place[1]):
                 return False
         return True
 
